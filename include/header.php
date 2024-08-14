1dc5dda398e85bb4b52a38e49f1483eee1e96bd3
nandan <nandannandaniya08@gmail.com>
2024-08-14 23:42:42 +0530

    convert file extension html to php & make a single header footer file

---
+++ include/header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Shree Dasharam Bram Components</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+  <nav class="navbar navbar-default navbar-trans navbar-expand-lg fixed-top">
+    <div class="container">
+      <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDefault" aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <a class="navbar-brand text-brand" href="index.php">Dasharam <span class="color-b">Brass</span></a>
+
+      <div class="navbar-collapse collapse justify-content-center" id="navbarDefault">
+        <ul class="navbar-nav">
+
+          <li class="nav-item">
+            <a class="nav-link active" href="index.php">Home</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link " href="about.php">About</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link " href="products.php">Products</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link " href="quality.php">Quality</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link " href="contact.php">Contact</a>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  </nav>
